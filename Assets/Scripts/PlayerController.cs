20b1a66f0e9c0190b7b9bb1af951aac66c4cc984
filatovTaras <Taras.w3c@gmail.com>
2021-06-17 11:45:38 +0600

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode forwardKeyCode = KeyCode.D;
+    [SerializeField]
+    private KeyCode backKeyCode = KeyCode.A;
+
+    private Animator animator;
+
+    static readonly int s_forwardParameterHash = Animator.StringToHash("Forward");
+    static readonly int s_backParameterHash = Animator.StringToHash("Back");
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(forwardKeyCode))
+        {
+            animator.SetBool(s_forwardParameterHash, true);
+        }
+
+        if (Input.GetKeyUp(forwardKeyCode))
+        {
+            animator.SetBool(s_forwardParameterHash, false);
+        }
+
+        if (Input.GetKeyDown(backKeyCode))
+        {
+            animator.SetBool(s_backParameterHash, true);
+        }
+
+        if (Input.GetKeyUp(backKeyCode))
+        {
+            animator.SetBool(s_backParameterHash, false);
+        }
+    }
+}
